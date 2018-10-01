1abb1a8f8907e66377b76539b95de7272090c8bb
user <madaragaile11@gmail.com>
2018-10-01 17:46:42 +0300

    20181001_17_46

---
+++ test_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+#http://www.cplusplus.com/reference/cstudio/printf/
+
+a = 10
+b = 1.56
+c = 'A'
+print("a = %d"%(a))
+print("b = %.4f"%(a))
+print("c = %s"%(c))
+a = 1726420
+b = 1234.6234682
+c = 'A'
+print("a = %d"%(a))
+print("b = %.3f"%(a))
+print("c = %s"%(c))
+
